8f557ba24a574eae2795847c8d79e401ac0b6300
Vasishath Kaushal <vasishath@gmail.com>
2018-11-18 01:11:29 +0530

    Add forbidden partitions list

---
+++ multirom/mr_forbidden_partitions.cpp
@@ -0,0 +1,7 @@
+#include <string.h>
+
+extern const char * const forbidden_images[] = {
+            "abl.elf", "bluetooth.img", "cmnlib.img", "dsp.img", "keymaster.img", "modem.img", "storsec.img", "xbl_config.img",
+            "aop.img", "cmnlib64.img",  "devcfg.img", "hyp.img", "logo.img", "qupfw.img", "tz.img", "xbl.img", "firmware-update",
+            NULL
+};
